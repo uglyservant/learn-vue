7a6d34791a5094ec7f256e31f85869a09273cb1b
uglyservant <uglyservant@yeah.net>
2019-12-10 18:33:38 +0800

    css-loader

--- learn-vuejs-02/03-webpack的loader/src/main.js
+++ learn-vuejs-02/03-webpack的loader/src/main.js
@@ -1,4 +1,8 @@
 const {add, mul} = require("./js/mathUtils.js");
+// 依赖CSS
+require("./css/normal.css");
 
 console.log(add(1, 2));
 console.log(mul(1, 2));
+
+
--- learn-vuejs-02/03-webpack的loader/webpack.config.js
+++ learn-vuejs-02/03-webpack的loader/webpack.config.js
@@ -6,5 +6,14 @@ module.exports = {
     // 动态获取文件路径
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // 顺序重要，从右向左
+        use: ["style-loader", "css-loader"]
+      }
+    ]
   }
 };
