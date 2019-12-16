58252925401c9519e846482d45a02822df3a7549
uglyservant <uglyservant@yeah.net>
2019-12-16 19:06:06 +0800

    today

---
+++ learnvuex/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h2>{{message}}</h2>
+    <h2>{{counter}}</h2>
+    <button @click="counter++">+</button>
+    <button @click="counter--">-</button>
+    <hello-vuex/>
+  </div>
+</template>
+
+<script>
+  import HelloVuex from "./components/HelloVuex";
+  export default {
+    name: 'App',
+    components: {HelloVuex},
+    data() {
+      return {
+        message: "我是App组件",
+        counter: 0
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
